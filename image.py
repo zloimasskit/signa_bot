a4b25b403bcdaf03ad71ba9ee72e9e3b2c3e60bd
zloimas <119732640+zloimasskit@users.noreply.github.com>
2025-09-15 22:00:59 +0300

    mvp

--- image.py
+++ image.py
@@ -1,6 +1,24 @@
-
-
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 
 class image:
+    @staticmethod
     def generate(charact, text):
-        return 0     
+        try:
+            font = ImageFont.truetype("arial.ttf", 40)
+        except:
+            font = ImageFont.load_default()
+
+        img = Image.open("assets/TRUMP.jpg")
+        draw = ImageDraw.Draw(img)
+
+        position = (260, 230)   # координаты текста
+        color = (0, 0, 0)     # черный текст
+
+        draw.text(position, text, fill=color, font=font)
+
+        img_buffer = BytesIO()
+        img.save(img_buffer, format="JPEG")
+        img_buffer.seek(0)
+
+        return img_buffer
--- main.py
+++ main.py
@@ -2,27 +2,32 @@ from dotenv import load_dotenv
 import os
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command 
-from aiogram import F
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
 from image import image
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=TOKEN)
 
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+class Stater(StatesGroup):
+    choosing_text = State()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    kb = [
-        [types.KeyboardButton(text="создать изображение")]
-    ]
+    kb = [[types.KeyboardButton(text="создать изображение")]]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-    await message.answer("""\n
-Привет🖐️ Ты запустил сигна_бот
-Этот бот позволит тебе создать свое изображение с текстом⚡                         """, reply_markup=keyboard)
-    
+    await message.answer(
+        "Привет🖐️ Ты запустил сигна_бот!\nЭтот бот позволит тебе создать своё изображение с текстом⚡",
+        reply_markup=keyboard
+    )
+
 @dp.message(F.text.lower() == "создать изображение")
 async def wit(message: types.Message):
     kb = [[types.KeyboardButton(text="трамп")]]
@@ -30,10 +35,19 @@ async def wit(message: types.Message):
     await message.reply("выбери персонажа", reply_markup=keyboard)
 
 @dp.message(F.text.lower() == "трамп")
-async def trum(message :types.Message):
-    await message.answer("введи текст")    
+async def trum(message: types.Message, state: FSMContext):
+    await message.answer("введи текст")
+    await state.set_state(Stater.choosing_text)
 
+@dp.message(Stater.choosing_text)
+async def proccess(message: types.Message, state: FSMContext):
+    name = message.text
+    await state.update_data(text=name)
 
+    img_buffer = image.generate("sda", text=name)  # должен возвращать BytesIO
+    await message.answer_photo(
+        types.BufferedInputFile(img_buffer.getvalue(), filename="image_with_text.jpg")
+    )
 
 async def main():
     await dp.start_polling(bot)
